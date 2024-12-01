87e608484307d1c9b0fba5922d4148997bf7cb62
rogerbaiges <rogerbaigestrilla@gmail.com>
2024-12-01 10:41:03 +0100

    MOD segment text

--- segmentor.py
+++ segmentor.py
@@ -948,7 +948,7 @@ class ImageSegmenter:
 		# Print the result to the console
 		print(f"Number of masks: {self.count_masks()}")
 
-		return f'There have been segmented a total of {self.count_masks()} classes of "{text}" in the image.', segmented_image_path
+		return f'There have been segmented a total of {self.count_masks()} of "{text}" in the image.', segmented_image_path
 	
 	def segment_unique(self, image, text):
 		"""
@@ -985,7 +985,6 @@ class ImageSegmenter:
 		color_RGB = {'red': (255, 0, 0), 'green': (0, 255, 0), 'blue': (0, 0, 255), 'yellow': (255, 255, 0), 'purple': (128, 0, 128), 'cyan': (0, 255, 255), 'white': (255, 255, 255), 'black': (0, 0, 0), 'orange': (255, 165, 0), 'pink': (255, 192, 203), 'brown': (165, 42, 42), 'gray': (128, 128, 128), 'lime': (0, 255, 0), 'maroon': (128, 0, 0), 'navy': (0, 0, 128), 'olive': (128, 128, 0), 'silver': (192, 192, 192), 'teal': (0, 128, 128)}	
 		if isinstance(color, str):
 			color = color_RGB.get(color.lower(), (255, 0, 0))
-		self.image = image
 		updated_image = self.apply_masks_to_image(color)
 		updated_image_path = image.replace(".jpg", "_color_changed.jpg").replace(".png", "_color_changed.png")
 		plt.imsave(updated_image_path, updated_image)
